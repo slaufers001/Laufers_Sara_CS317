bd70b4cfb7dc90bc4229159f2f181fef556ca0b8
slaufers001 <slaufers001@csbsju.edu>
2019-02-25 20:58:56 +0000

    Setting up

--- Solitaire/MVP/Card.cpp
+++ Solitaire/MVP/Card.cpp
@@ -1,1 +1,24 @@
-// hi
+#include <stdlib.h>     /* srand, rand */
+#include <iostream>
+
+class Card {
+// =================== INSTANCES  ===================
+    public:
+    int num;            // the number/rank of the card
+    std::string suit;   // the type/suit of the card
+    bool isVis;         // whether the card is face up (true) or face down (false)
+
+// =================== CONSTRUCTOR ===================
+    Card(int number, std::string cardSuit, bool visible){
+        num = number;
+        suit = cardSuit;
+        isVis = visible;
+    }
+
+// =================== METHODS ===================
+
+// ------------------- ACCESSORS -------------------
+    int getNum(){
+        return num;
+    }
+};
